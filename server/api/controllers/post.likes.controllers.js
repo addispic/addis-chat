545a5fca3729fff82069214f0fd921b04093ca20
Adisie Fenta <adisiefenta23@gmail.com>
2024-09-15 17:58:00 +0300

    chnages

--- server/api/controllers/post.likes.controllers.js
+++ server/api/controllers/post.likes.controllers.js
@@ -1,6 +1,6 @@
 // models
 // post
-const Post = require('../models/post.schema')
+const Post = require("../models/post.schema");
 // post like
 const PostLike = require("../models/post.likes.schema");
 
@@ -26,32 +26,29 @@ const addNewPostLike = async (req, res) => {
     const { postId } = req.params;
     const userId = req.user._id;
 
-    const isPostExist = await Post.findById(postId) 
+    const isPostExist = await Post.findById(postId);
 
     // is post exist
-    if(!isPostExist){
-        return res.status(400).json({
-            error: 'post not found',
-        })
+    if (!isPostExist) {
+      return res.status(400).json({
+        error: "post not found",
+      });
     }
 
     // is user already like
-    const isUserAlreadyLike = await PostLike.find({userId})
+    const isUserAlreadyLike = await PostLike.find({ userId });
 
-    if(isUserAlreadyLike?.length > 0){
-        return res.status(400).json({
-            error: 'user already like the post'
-        })
+    if (isUserAlreadyLike?.length > 0) {
+      return res.status(400).json({
+        error: "user already like the post",
+      });
     }
 
-    const newPostLike = await PostLike.create({userId,postId})
-
-
+    const newPostLike = await PostLike.create({ userId, postId });
 
     return res.status(200).json({
       newPostLike,
     });
-
   } catch (err) {
     return res.status(200).json({
       error: "add new post like error",
@@ -62,31 +59,30 @@ const addNewPostLike = async (req, res) => {
 // delete post like
 const deletePostLike = async (req, res) => {
   try {
-    const {_id} = req.params 
-    const isPostLikeExist = await PostLike.findById(_id) 
-    if(!isPostLikeExist){
-        return res.status(400).json({
-            error: 'post like not found',
-        })
+    const { _id } = req.params;
+    const isPostLikeExist = await PostLike.findById(_id);
+    if (!isPostLikeExist) {
+      return res.status(400).json({
+        error: "post like not found",
+      });
     }
 
     // is authorized
-    console.log(isPostLikeExist)
-    if(!isPostLikeExist?.userId?.equals(req.user._id)){
-        return res.status(400).json({
-            error: 'unauthorized to delete the like',
-        })
+    if (!isPostLikeExist?.userId?.equals(req.user._id)) {
+      return res.status(400).json({
+        error: "unauthorized to delete the like",
+      });
     }
-    await PostLike.findByIdAndDelete(_id)
+    await PostLike.findByIdAndDelete(_id);
     return res.status(200).json({
-        _id,
-        message: 'post like deleted',
-    })
+      _id,
+      message: "post like deleted",
+    });
   } catch (err) {
-    conso
+    conso;
     return res.status(400).json({
-        error: 'delete post like error',
-    })
+      error: "delete post like error",
+    });
   }
 };
 
